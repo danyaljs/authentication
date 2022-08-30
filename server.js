ffa387d3612fa27ffe7d946491ef746acfed8d3b
danyal-jsh <d.jiryaee@gmail.com>
2022-08-30 21:44:22 +0430

    config express and httpServer,connect to DB

--- server.js
+++ server.js
@@ -1,12 +1,51 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 3000,
+    mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1/authenticationDB',
+    helmet = require('helmet'),
+    http = require('http'),
+    cors = require('cors');
 
-const port = process.env.PORT || 3000;
-const app = express();
-
+const mongoose = require('mongoose');
+const mongooseOptions = {
+    socketTimeoutMS: 30000,
+    keepAlive: true
+};
+mongoose.connect(mongoUrl, mongooseOptions).then(() => {
+    console.log('successfully connect to DB');
+}).catch(error => {
+    console.log(error)
+});
 
 app.use(helmet);
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.listen(port,console.log(`server is running on port : ${port}`));
+const routes = require('./routes/user');
+routes(app);
+
+const server = http.createServer(app);
+server.listen(port);
+server.on('listening', () => {
+    console.log(`server is running on port : ${port}`)
+});
+server.on('error', (error) => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    // handle specific listen error with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error("API: Port '%s' requires elevated privileges", port);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error("API: Port '%s' is already in use", port);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+});
