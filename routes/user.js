921ff4c9692ce62129ff4a9d9a57cd254a5455b6
danyal-jsh <d.jiryaee@gmail.com>
2022-09-10 21:10:31 +0430

    add route to get profile info

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,6 @@ module.exports = function (app) {
     app.route('/auth/register').post(userHandlers.register);
 
     app.route('/auth/login').post(userHandlers.login);
+
+    app.route('/auth/profile').get(userHandlers.loginRequired,userHandlers.getProfile)
 };
